49f1ef471373778a4d213aa68e8652bebd057261
Emil <bmachaku@gmail.com>
2017-02-07 15:16:03 +0300

    add loaddata command

--- ckan_loaddata/cli.py
+++ ckan_loaddata/cli.py
@@ -1,15 +1,31 @@
 # -*- coding: utf-8 -*-
 
 import click
+import yaml
+from ckan_loaddata.ckan import CKAN
 
+@click.group()
+def cli():
+    pass
 
 @click.command()
-def main(args=None):
-    """Console script for ckan_loaddata"""
-    click.echo("Replace this message by putting your code into "
-               "ckan_loaddata.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.argument('task', type=click.File('rb'))
+def loaddata(task):
+    """Upload data to a CKAN instance."""
+    click.echo('Upload data to CKAN')
+    conf = yaml.load(task)
+    ckan = CKAN(address=conf.get('address'),
+                apikey=conf.get('apikey'),
+                user_agent=conf.get('user_agent'))
+    for resource in conf.get('resources', []):
+        ckan.resource_create_from_url(
+            resource.get('url'),
+            input_params=resource.get('input', {}),
+            output_params=resource.get('output', {}))
+
+
+cli.add_command(loaddata)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
